751e7096513a2e30370a74e53b82f5c7ec88008e
Kaosboy16 <72620115+Kaosboy16@users.noreply.github.com>
2021-05-17 10:38:07 +0200

    Add files via upload

---
+++ click me twice.bat
@@ -0,0 +1,24 @@
+@echo off
+color 07
+title totally a virus
+echo do you really want to proceed?
+pause
+cls
+color 87
+echo stop
+pause
+cls
+color c0
+echo I
+echo SAID
+echo STOP!
+pause
+cls
+color 40
+echo if you press any button again,
+echo then your gonna get the virus!
+pause
+cls
+start https://www.youtube.com/watch?v=QtBDL8EiNZo
+echo told you!
+pause
